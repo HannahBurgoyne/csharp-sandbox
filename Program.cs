062e18faba5f51524fab7d10de97776be70a55c2
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2023-11-08 12:27:25 +1300

    Validate user input for new pet

--- Program.cs
+++ Program.cs
@@ -146,6 +146,27 @@ do
 
       while (anotherPet == "y" && petCount < maxPets)
       {
+        bool validEntry = false; 
+
+        do {
+          Console.WriteLine("\n\rEnter 'dog' or 'cat' to begin a new entry");
+          readResult = Console.ReadLine();
+
+          if (readResult != null)
+          {
+            animalSpecies = readResult.ToLower();
+
+            if (animalSpecies != "dog" && animalSpecies != "cat")
+            {
+              // Console.WriteLine($"You entered: {animalSpecies}.");
+              validEntry = false; 
+            }
+            else 
+            {
+              validEntry = true;
+            }
+          } 
+        } while (validEntry == false);
         petCount = petCount + 1; 
 
         // check max pet limit before prompting user to add another pet
